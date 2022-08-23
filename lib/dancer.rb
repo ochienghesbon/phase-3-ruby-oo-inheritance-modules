48542ca0d8342c88fb649f73924dada6f3c8a495
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-08-23 19:21:07 +0300

    commit tree 24a1e4755b789fd7f36d3e9ed07bca14a17d4d0e

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,14 @@
-require_relative './dance_module'
-require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+class Dancer
+    
+    include FancyDance::InstanceMethods
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    extend FancyDance::ClassMethods
+end
